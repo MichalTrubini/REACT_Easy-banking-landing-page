d89595ed1c0b417a3f671ece2469331ba2f57788
Michal Trubini <mchl.trubini@gmail.com>
2022-07-30 10:28:44 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 import MobileMenu from './components/header/MobileMenu';
-import useWindowDimensions from './functions/WindowDimensions';
 import { useState } from 'react';
 
 function App() {
   
-  const {width} = useWindowDimensions(); 
-
   const [menuVisible, setMenuVisible] = useState(false);
  
   const showMenuHandler = () => {
     setMenuVisible(PrevVal => !PrevVal)
   }
 
-  if (menuVisible === true) {document.body.style.overflow = "hidden"}
-  else {{document.body.style.overflow = "visible"}};
+  menuVisible === true ? document.body.style.overflow = "hidden" : document.body.style.overflow = "visible";
 
   return (
       <div className="wrapper">
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -35,33 +35,35 @@ const Footer = () => {
 
     return ( 
         <footer className="footer">
-            <div className="footer__container">
-                <div className="footer__wrapper">
-                    <div className="footer__logo-social">
-                        <img className="footer__logo" src={logo} alt="Easybank" />
-                        <div className="footer__social">
-                            { social.map(item => <img key={item.id} src={item.src} alt={item.alt}/>)}
+            <div className="footer__main">
+                <div className="footer__container">
+                    <div className="footer__wrapper">
+                        <div className="footer__logo-social">
+                            <img className="footer__logo" src={logo} alt="Easybank" />
+                            <div className="footer__social">
+                                { social.map(item => <img key={item.id} src={item.src} alt={item.alt} className="footer__social-img"/>)}
+                            </div>
                         </div>
                     </div>
+                    <div className="footer__wrapper">
+                        <ul className="footer__list">
+                            <li className="footer__list-item">About us</li>
+                            <li className="footer__list-item">Contact</li>
+                            <li className="footer__list-item">Blog</li>
+                        </ul>
+                    </div>
+                    <div className="footer__wrapper">
+                        <ul className="footer__list">
+                            <li className="footer__list-item">Careers</li>
+                            <li className="footer__list-item">Support</li>
+                            <li className="footer__list-item">Privacy Policy</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="footer__wrapper">
-                    <ul className="footer__list">
-                        <li className="footer__list-item">About us</li>
-                        <li className="footer__list-item">Contact</li>
-                        <li className="footer__list-item">Blog</li>
-                    </ul>
+                <div className="footer__container footer__bottom">
+                    <Button>Request Invite</Button>
+                    <p className="footer__copyright">© Easybank. All Rights Reserved</p>
                 </div>
-                <div className="footer__wrapper">
-                    <ul className="footer__list">
-                        <li className="footer__list-item">Careers</li>
-                        <li className="footer__list-item">Support</li>
-                        <li className="footer__list-item">Privacy Policy</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="footer__container footer__bottom">
-                <Button>Request Invite</Button>
-                <p className="footer__copyright">© Easybank. All Rights Reserved</p>
             </div>
         </footer>
 
